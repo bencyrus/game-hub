0afaf879cc0fe0d8f8ed47fc78232f9f17cdb7c9
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-05-16 09:18:51 -0400

    built game cards

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <Grid
             templateAreas={{
                 base: `"nav" "main"`,
-                lg: `"nav nav" "main aside"`,
+                lg: `"nav nav" "aside main"`,
             }}
         >
             <GridItem gridArea="nav">
