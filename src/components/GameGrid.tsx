cd4c92366d6ca7aabd240c239c8461cecb58c815
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-05-20 22:17:13 -0400

    refactored gane grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,27 +14,28 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
 
     const Skeletons = [1, 2, 3, 4, 5, 6]
 
+    if (error) {
+        return <Text>{error}</Text>
+    }
+
     return (
-        <div>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid
-                columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-                spacing={6}
-                padding="10px"
-            >
-                {loading &&
-                    Skeletons.map((s) => (
-                        <GameCardContainer key={s}>
-                            <GameCardSkeleton />
-                        </GameCardContainer>
-                    ))}
-                {data.map((game) => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+            spacing={6}
+            padding="10px"
+        >
+            {loading &&
+                Skeletons.map((s) => (
+                    <GameCardContainer key={s}>
+                        <GameCardSkeleton />
                     </GameCardContainer>
                 ))}
-            </SimpleGrid>
-        </div>
+            {data.map((game) => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
     )
 }
 
